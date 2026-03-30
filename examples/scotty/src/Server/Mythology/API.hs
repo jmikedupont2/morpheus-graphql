57252de77bdeef9b59f10d793ce3064f47ed8fa1
mike dupont <gemini-agent-47@solfunmeme.com>
2026-03-30 11:53:29 -0400

    Add MetaCoq/Introspector integration on top of v0.28.5
    - MetaCoq TestMeta, TestMeta2, TestMeta3 modules
    - Modified Mythology API and Character for introspector

--- examples/scotty/src/Server/Mythology/API.hs
+++ examples/scotty/src/Server/Mythology/API.hs
@@ -1,13 +1,49 @@
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE StandaloneDeriving                  #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses  #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Server.Mythology.API
   ( app,
   )
 where
+
+import Data.Morpheus.Ext.Result(GQLResult)
+import Data.Morpheus.Types.Internal.AST.Fields(ArgumentsDefinition)
+import Data.Morpheus.Types.Internal.AST.Stage(CONST)
+import Data.Morpheus.Server.CodeGen.Internal
+import Data.Morpheus.Server.Types
+-- import Globals.GQLScalars (ScalarPower)
+
+import Data.Morpheus.Types.Internal.AST.OperationType(QUERY)
+import Data.Morpheus.App.Internal.Resolving.Types(ResolverValue)
+import Data.Morpheus.App.Internal.Resolving.Resolver(Resolver)
+import Data.Morpheus.App.Internal.Resolving.MonadResolver
+import Data.Morpheus.App.Internal.Resolving.ResolveValue
+import Data.Morpheus.App.Internal.Resolving.ResolverState
+
+import Data.Morpheus.Generic( GRep,deriveTypeValue,deriveTypeDefinition )
+--    GRepCons (..),
+--    GRepField (..),
+--    GRepFun (..),
+--    GRepType (..),
+--    deriveType,
+--  )
+
+import Data.Morpheus.Server.Deriving.Utils.Use
+import Data.Morpheus.Server.Deriving.Utils.GScan
+import Data.Morpheus.Server.Deriving.Utils.Types
+
+-- import Data.Morpheus.Server.Deriving.Utils
+
 
 import Data.Morpheus
   ( App,
@@ -22,17 +58,20 @@ import Data.Morpheus.Types
     liftEither,
   )
 import Data.Text (Text)
-import GHC.Generics (Generic)
+import Data.Morpheus.Server.Types.GQLType
+import GHC.Generics (Generic,Rep)
 import Server.Mythology.Character
   ( Deity,
     Human,
     PersonGuard,
     dbDeity,
+    dbDeityStory,
     resolvePersons,
     someDeity,
     someHuman,
   )
 import Server.Mythology.Place (City)
+
 
 data Character m
   = CharacterHuman (Human m) -- Only <tyconName><conName> should generate direct link
@@ -54,6 +93,10 @@ data Query m = Query
   { deity :: DeityArgs -> m Deity,
     character :: [Character m],
     persons :: [PersonGuard m]
+--    deity_story :: DeityArgs -> m Ident
+  --Kername
+--    deity_story :: DeityArgs -> m Global_declarations 
+               
   }
   deriving (Generic, GQLType)
 
@@ -67,7 +110,14 @@ resolveDeity :: DeityArgs -> ResolverQ e IO Deity
 resolveDeity DeityArgs {name, bornPlace} =
   liftEither $ dbDeity name bornPlace
 
-resolveCharacter :: (Applicative m) => [Character m]
+--resolveDeityStory :: DeityArgs -> ResolverQ e IO Global_declarations 
+--resolveDeityStory DeityArgs {name, bornPlace}=
+-- liftEither $ dbDeityStory name bornPlace
+
+--dbDeity :: Text -> Maybe City -> IO (Either String Deity)
+
+
+resolveCharacter :: Applicative m => [Character m]
 resolveCharacter =
   [ CharacterHuman someHuman,
     CharacterDeity someDeity,
@@ -80,6 +130,8 @@ resolveCharacter =
     SomeMutli 21 "some text",
     Zeus,
     Cronus
+    -- Introspector
+    --Introspector rec_def_term
   ]
 
 rootResolver :: RootResolver IO () Query Undefined Undefined
@@ -90,8 +142,71 @@ rootResolver =
           { deity = resolveDeity,
             character = resolveCharacter,
             persons = resolvePersons
+--            , deity_story = resolveDeityStory
           }
     }
 
+-- instance Data.Morpheus.Server.Deriving.Utils.GRep.GRep
+--          GQLType
+--          (Data.Morpheus.Server.Types.GQLType.GQLResolver
+--            (Data.Morpheus.App.Internal.Resolving.Resolver.Resolver
+--              Data.Morpheus.Types.Internal.AST.OperationType.QUERY
+--              ()
+--              IO))
+--   (Data.Morpheus.App.Internal.Resolving.Resolver.Resolver
+--     Data.Morpheus.Types.Internal.AST.OperationType.QUERY
+--     ()
+--     IO
+--     (Data.Morpheus.App.Internal.Resolving.Types.ResolverValue
+--       (Data.Morpheus.App.Internal.Resolving.Resolver.Resolver
+--         Data.Morpheus.Types.Internal.AST.OperationType.QUERY
+--         ()
+--         IO)))
+--          (GHC.Generics.Rep (Prod Modpath Ident))
+
+-- instance (GRep
+--                          GQLType
+--                          (Data.Morpheus.Server.Types.GQLType.GQLResolver
+--                             (Resolver QUERY () IO))
+--                          (Resolver QUERY () IO (ResolverValue (Resolver QUERY () IO)))
+--                          (GHC.Generics.Rep (Prod Modpath Ident)))
+
+--foo ::  (Rep (Prod Modpath Ident))
+--foo = _
+--instance GRep GQLType 
+  --  (GQLResolver (Resolver QUERY () IO))
+  --  (Resolver QUERY () IO (ResolverValue (Resolver QUERY () IO)))
+  --  (Rep (Prod Modpath Ident)) where
+  -- deriveTypeValue  =1  -- :: GRepFun gql c Identity v -> f a -> (Bool, GRepCons v)
+  -- deriveTypeDefinition  =1 -- :: GRepFun gql c Proxy v -> proxy f -> [GRepCons v]
+
+
+  
 app :: App () IO
 app = deriveApp rootResolver
+
+
+
+--instance GRep (GraphQLType Kername)
+--   (GQLResolver QUERY)
+--   (Resolver QUERY (ResolverValue QUERY))
+--   Kername where
+--  f = _
+
+-- instance GRep (GraphQLType (Prod Kername Kername)) 
+--                     (GQLResolver QUERY)
+--                     (Resolver QUERY (ResolverValue QUERY))
+--                     (Prod Kername Kername)
+-- deriving via GenericGRepInstance (Prod Kername Kername)
+
+--deriving instance GQLType (Prod Global_env Term)
+--instance GQLType Global_env where
+--  type KIND Global_env = TYPE
+
+-- instance GRep (
+--            GQLType
+--            GQLType
+--            (Data.Morpheus.Ext.Result.GQLResult
+--             (Data.Morpheus.Types.Internal.AST.Fields.ArgumentsDefinition
+--               Data.Morpheus.Types.Internal.AST.Stage.CONST))
+--            (Rep (List (Branch Term))))
--- examples/scotty/src/Server/Mythology/Character.hs
+++ examples/scotty/src/Server/Mythology/Character.hs
@@ -5,12 +5,13 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies #-}
-
+{-# LANGUAGE FlexibleInstances #-}
 module Server.Mythology.Character
   ( Deity (..),
     dbDeity,
     Human (..),
     someHuman,
+    dbDeityStory,
     someDeity,
     Person,
     PersonGuard,
@@ -26,6 +27,10 @@ import Server.Mythology.Place
     Realm (..),
   )
 
+import Server.MetaCoq.TestMeta
+import Server.MetaCoq.TestMeta2
+import Server.MetaCoq.TestMeta3
+
 newtype Person = Person {name :: Text}
   deriving (Generic, GQLType)
 
@@ -33,19 +38,29 @@ data Deity = Deity
   { name :: Text, -- Non-Nullable Field
     power :: Maybe Text, -- Nullable Field
     realm :: Realm,
-    bornAt :: Maybe City
+    bornAt :: Maybe City,
+   storyOf :: Mutual_inductive_body
+       -- Kername
+       -- Ident
+       -- Kername (Prod Modpath Ident)))
+   -- Global_declarations -- Prod Kername Global_decl
+    -- Global_declarations -> Global_env -> Maybe Global_env
   }
   deriving (Generic, GQLType)
+
+--instance GQLType (Prod Modpath Ident) where
+--  directives _ = _
 
 data Human m = Human
   { name :: m Text,
     bornAt :: m City
+--    , storyOf :: m Global_declarations
   }
   deriving (Generic, GQLType)
 
 type PersonGuard m = TypeGuard Person (UnionPerson m)
 
-resolvePersons :: (Applicative m) => [PersonGuard m]
+resolvePersons :: Applicative m => [PersonGuard m]
 resolvePersons = ResolveType <$> [UnionPersonDeity someDeity, UnionPersonHuman someHuman]
 
 data UnionPerson m
@@ -53,8 +68,17 @@ data UnionPerson m
   | UnionPersonHuman (Human m)
   deriving (Generic, GQLType)
 
-someHuman :: (Applicative m) => Human m
-someHuman = Human {name = pure "Odysseus", bornAt = pure Ithaca}
+getTerm :: Global_declarations
+getTerm = case rec_def_term of {
+  (Pair (Mk_global_env a b c) d) -> b
+}
+
+someHuman :: Applicative m => Human m
+someHuman = Human {
+  name = pure "Odysseus",
+  bornAt = pure Ithaca
+  --, storyOf = pure getTerm  
+  }
 
 someDeity :: Deity
 someDeity =
@@ -62,6 +86,7 @@ someDeity =
     { name = "Morpheus",
       power = Just "Shapeshifting",
       realm = Dream,
+--      storyOf = getTerm,
       bornAt = Nothing
     }
 
@@ -73,5 +98,16 @@ dbDeity _ bornAt =
         { name = "Morpheus",
           power = Just "Shapeshifting",
           realm = Dream,
+--          storyOf = getTerm,
           bornAt
         }
+dbDeityStory :: Text -> Maybe City -> IO (Either String Global_declarations )
+dbDeityStory _ storyOf =
+  return $ Right $ case rec_def_term of {
+    (Pair (Mk_global_env a b c) d) -> b
+  --Mk_global_env T35 Global_declarations T37
+  --(Prod Global_env Term)
+--  (Pair a c)   -> a;
+    --(Prod global_env _) -> global_env;
+--    _ -> "error";
+  }
